b609e55972979a0d39185a8c3b31262d4f7667f0
Mohsin Reza <engr.mmreza@gmail.com>
2022-03-29 12:53:40 +0200

    LVPD-3086: fix test

--- bbq/sample_messages.go
+++ bbq/sample_messages.go
@@ -5,37 +5,11 @@ type MessageA struct {
 	B int32
 	C *MessageB
 	D []*MessageB
-	E MessageC
 }
 
 type MessageB struct {
 	A int32
 	B map[string]string
-}
-
-type MessageC int32
-
-const (
-	MessageC_UNSPECIFIED MessageC = 0
-	MessageC_OTHER       MessageC = 1
-)
-
-// Enum value maps for Platform.
-var (
-	MessageC_name = map[int32]string{
-		0: "UNSPECIFIED",
-		1: "OTHER",
-	}
-	MessageC_value = map[string]int32{
-		"UNSPECIFIED": 0,
-		"OTHER":       1,
-	}
-)
-
-func (x MessageC) Enum() *MessageC {
-	p := new(MessageC)
-	*p = x
-	return p
 }
 
 type BytesCodec struct{}
