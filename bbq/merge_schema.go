ea4d0e1d51378a79cd4f2334acd0b714f7507a74
Mohsin Reza <engr.mmreza@gmail.com>
2022-03-31 11:18:03 +0200

    LVPD-3086: rename var name

--- bbq/merge_schema.go
+++ bbq/merge_schema.go
@@ -9,24 +9,24 @@ func mergeFieldsToBqSchema(existingSchema, newSchema bigquery.Schema) bigquery.S
 			continue
 		}
 
-		getChildSchema := getSchemaByName(existingSchema, fieldSchema.Name)
-		if getChildSchema != nil {
-			getChildSchema.Schema = mergeFieldsToBqSchema(getChildSchema.Schema, fieldSchema.Schema)
+		childFieldSchema := getFieldSchemaByName(existingSchema, fieldSchema.Name)
+		if childFieldSchema != nil {
+			childFieldSchema.Schema = mergeFieldsToBqSchema(childFieldSchema.Schema, fieldSchema.Schema)
 		}
 	}
 	return existingSchema
 }
 
-func isNameExists(s bigquery.Schema, name string) bool {
-	for _, i := range s {
-		if i.Name == name {
+func isNameExists(schema bigquery.Schema, name string) bool {
+	for _, fieldSchema := range schema {
+		if fieldSchema.Name == name {
 			return true
 		}
 	}
 	return false
 }
 
-func getSchemaByName(schema bigquery.Schema, name string) *bigquery.FieldSchema {
+func getFieldSchemaByName(schema bigquery.Schema, name string) *bigquery.FieldSchema {
 	for _, fieldSchema := range schema {
 		if fieldSchema.Name == name {
 			return fieldSchema
